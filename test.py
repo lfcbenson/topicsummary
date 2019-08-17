679fa54c61d5d92b17267fe440c1dd75f37348d4
unknown <bensonandrei91@gmail.com>
2019-08-17 14:38:20 +0530

    new file

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 def addone(num):
     return num+1
 
+def square(num):
+    return num**2
+
 if __name__ == "__main__":
     print(addone(1))
---
+++ test2.py
@@ -0,0 +1,5 @@
+def addtwo(num):
+    return num+2
+
+if __name__ == "__main__":
+    print(addtwo(1))dir
